91f7cff414b3a939127b52240dbece333ad4f84c
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-11-22 15:51:38 -0500

    rename cost variables

--- data-raw/costs/capitalcosts.R
+++ data-raw/costs/capitalcosts.R
@@ -2,10 +2,6 @@
 prep.capitalcosts <- function(aeofile, gdpdeflator)
 {
 
-
-  # To-Do -------------------------------------------------------------------
-
-
   ## DATA
   overnight <- read.delim(aeofile) %>%
     select(year, cost.year, overnight_category, base.overnight, variable.o.m, fixed.o.m) %>%
@@ -16,15 +12,17 @@ prep.capitalcosts <- function(aeofile, gdpdeflator)
 
   ## UNIT CONVERSIONS
   overnight <- overnight %>%
-    mutate(fixed.o.m = fixed.o.m/1000/8760,# /kWyr -> /MWh
-           base.overnight = base.overnight/1000) # /kW -> MW
+    mutate(fixed.o.m = fixed.o.m/1000,# /kW*yr -> /MW*yr
+           base.overnight = base.overnight/1000) # /kW -> /Mw
 
   ## VALUE ADJUSTMENT
   capitalcosts <- overnight %>%
     inner_join(gdpdeflator) %>%
-    mutate(base.overnight = deflator * base.overnight,
-           variable.o.m = deflator * variable.o.m,
-           fixed.o.m = deflator * fixed.o.m) %>%
-    select(-cost.year, -deflator)
+    mutate(overnight = deflator * base.overnight,
+           om.var = deflator * variable.o.m,
+           om.fixed = deflator * fixed.o.m) %>%
+    select(year, overnight_category, overnight, om.var, om.fixed)
+
+  capitalcosts
 
 }
