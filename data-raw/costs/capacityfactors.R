784858045ad24d7ab82aee713d100bf4c4995cb7
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-11-27 18:02:40 -0500

    ungroup final df

--- data-raw/costs/capacityfactors.R
+++ data-raw/costs/capacityfactors.R
@@ -4,7 +4,8 @@ calc.capacityfactors <- function(generators, plantgeneration)
     select(year, summer_capacity, utility_code, plant_code, generator_code, prime_mover, fuel) %>%
     rename(capacity = summer_capacity) %>%
     group_by(year, utility_code, plant_code, prime_mover, fuel) %>%
-    summarise(capacity=sum(capacity))
+    summarise(capacity=sum(capacity)) %>%
+    ungroup()
 
   plantgeneration <- plantgeneration %>%
     select(-consumption)
@@ -15,5 +16,7 @@ calc.capacityfactors <- function(generators, plantgeneration)
     mutate(capacityfactor = generation/potentialgeneration) %>%
     filter(capacityfactor > 0 & capacityfactor < 1) %>%
     select(year, utility_code, plant_code, prime_mover, fuel, capacityfactor)
+
+  capacityfactors
 
 }
