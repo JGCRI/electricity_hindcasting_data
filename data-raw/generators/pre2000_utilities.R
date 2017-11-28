a28373ced82fd07bea3d3c593313e2e9f1cb983a
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-11-28 12:04:14 -0500

    begin rescraping form860

--- data-raw/generators/pre2000_utilities.R
+++ data-raw/generators/pre2000_utilities.R
@@ -1,39 +1,49 @@
-# 1990-2000 Utilities Form860 ------------------------------
-library(stringr)
-library(xlsx)
-startingDir<- "data-raw/generators/Utilities (1990-2000)"
-dirs<-list.dirs(startingDir, full.names=FALSE)
-cols <- list()
+prep.generators.90to00 <- function(startingDir, filt, all)
+  {
+  dirs<-list.dirs(startingDir, full.names=FALSE)
+  cols <- list()
 
-for (i in seq(1:length(dirs)) ) {
-  #i <- 7
-  dir <- dirs[i]
-  year <- str_extract(dir, "[1|2][0-9]{3}" )
-  if (is.na(year)) {next}
+  # each year has files stored in its own directory underneath startingDir
+  for (i in seq(1:length(dirs)) ) {
+    dir <- dirs[i]
+    year <- str_extract(dir, "[1|2][0-9]{3}" )
+    if (is.na(year)) {next}
 
-  filez <- list.files(paste(startingDir, dir, sep="/"))
-  group1 <- c(1990, 1991)
-  group2 <- c(1992, 1993, 1994, 1995, 1996)
-  group3 <- c(1997)
-  group4 <- c(1998, 1999, 2000)
-  if (year %in% group1 | year %in% group2) {pattern <- "TYPE3"}
-  if (year %in% group3) {pattern <- "GENERTOR"}
-  if (year %in% group4) {pattern <- "ExistingGenerators"}
-  genfile <- str_subset(filez, pattern)
+    # get that year's files. naming convention differs
+    filez <- list.files(paste(startingDir, dir, sep="/"))
+    group1 <- c(1990, 1991)
+    group2 <- c(1992, 1993, 1994, 1995, 1996)
+    group3 <- c(1997)
+    group4 <- c(1998, 1999, 2000)
+    if (year %in% group1 | year %in% group2) {file <- "TYPE3"}
+    if (year %in% group3) {file <- "GENERTOR"}
+    if (year %in% group4) {file <- "ExistingGenerators"}
+    genfile <- str_subset(filez, file)
 
-  data.raw <- read.xlsx2(paste(startingDir, dir, genfile, sep="/"), 1)
-  if (year %in% group1) {
+    data.raw <- read_excel(paste(startingDir, dir, genfile, sep="/"), sheet=1)
+    names(data.raw) <- names(data.raw) %>%
+      toupper() %>%
+      gsub("AND", "&", .) %>%
+      gsub("[_\\.]", "", .)
+
+    if (filt) {
+      data.filt <- data.raw %>%
+        filter(matches("MULTIGEN") == "") %>%
+        select(UTILITYCODE, PLANTCODE, GENERATORCODE, PRIMEMOVER, ENERGYSOURCE1, # ID info
+               NAMEPLATECAPACITY, SUMMERCAPABILITY, WINTERCAPABILITY, HEATRATE, # system values
+               SERVICETYPE, STATUSCODE, STATUSCODE1, NOTES, SUMMERCAPABILITYFLAG, WINTERCAPABILITYFLAG,# descriptors
+               INSERVICEYEAR, STARTOPERATIONYEAR) # years
+      # 90 NAMEPLATECAPACITY in kW
+    }
+
+    if (all) {
+      data.allfilt <- data.raw %>%
+        filter(matches("MULTIGEN") == "") %>%
+        select(matches("UTIL"), PLANTCODE )
+    }
+
+    # store data cols
     cols[[genfile]] <- names(data.raw)
   }
-
-  else(break)
+  cols
 }
-
-if (year == 1990) {cols <- append(cols, names(data.raw)) %>% unlist()}
-else if (year %in% group1) {
-  if(any(!names(data.raw) %in% cols)) {
-    print(year)
-    print(names(data.raw)[!names(data.raw) %in% cols])
-  }
-}
-else (break)
