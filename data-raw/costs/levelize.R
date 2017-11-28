7de6bb75dd2d44abd1069a33ff9871e62604a7e8
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-11-28 12:07:41 -0500

    match suitable generation numbers to weight levcost

--- data-raw/costs/levelize.R
+++ data-raw/costs/levelize.R
@@ -1,4 +1,4 @@
-calc.levelizedcosts <- function(capfactor, capcosts, map, fcr, gen)
+calc.levelizedcosts <- function(capfactor, capcosts, map, fcr)
 {
 
   levcst <- capfactor %>%
@@ -8,23 +8,22 @@ calc.levelizedcosts <- function(capfactor, capcosts, map, fcr, gen)
     mutate(overnight = fcr * overnight / capacityfactor,
            om.fixed = om.fixed / capacityfactor,
            levcost = overnight + om.fixed + om.var) %>%
-    select(-capacityfactor, -overnight, -om.var, -om.fixed)
+    select(year, utility_code, plant_code, prime_mover, fuel, overnight_category, fuel_general, levcost)
 
   # when grouped by ID's (year, util, plnt, oc, fg), levcst has duplicate rows
   # this is from multiple pm-f choices mapping to the same oc
-  # must collapse these dupicate rows by taking weighted average across multiple instances of oc-fg choice
-  # weight by share of total generation (for year, util, plnt, oc, fg ID's)
+  # must collapse these duplicate rows by taking weighted average across multiple instances of oc-fg choice
+  # weight by share of total generation (for ID's year, util, plnt, oc, fg)
   # generation.weight = generation/generation.total
 
   # generation (using oc-fg ID's)
-  # due to inconsistent/erroneous recordkeeping, some plants generated more than their recorded capacity allows.
-  # in other cases, generators produced negative energy (used power to stay online but weren't utilized)
-  # both instances were filtered out in producing the capacityfactors data, therefore we will use only generation data
-  # consistent with the capacityfactors data. this is what left_join(capfactor, gen) accomplishes
+  # see capacityfactors.R for documentation of wonky numbers from that calculation.
+  # we only want to use generation weights that contributed to acceptable capacityfactors,
+  # so we take the 'generation' col from the calculated capacityfactors df
   gen <- capfactor %>%
-    left_join(gen, by=c("year", "utility_code" ,"plant_code", "prime_mover", "fuel")) %>%
+    select(year, utility_code, plant_code, prime_mover, fuel, generation) %>%
     left_join(map, by=c("prime_mover", "fuel")) %>%
-    select(year, utility_code, plant_code, overnight_category, fuel_general, generation)
+    select(-prime_mover, -fuel)
 
   # generation.total (using oc-fg ID's)
   totalgen <- gen %>%
@@ -33,15 +32,14 @@ calc.levelizedcosts <- function(capfactor, capcosts, map, fcr, gen)
     ungroup()
 
   # generation.weight
-  levcst <- levcst %>%
+  levcst.weighted <- levcst %>%
     left_join(gen, by=c("year", "utility_code", "plant_code", "overnight_category", "fuel_general") ) %>%
     left_join(totalgen, by=c("year", "utility_code", "plant_code", "overnight_category", "fuel_general")) %>%
     mutate(generation.weight =  generation/generation.total) %>%
     # collapse duplicate rows for ID's (year, util, plnt, oc, fg)
     group_by(year, utility_code, plant_code, overnight_category, fuel_general) %>%
     summarise(levcost = weighted.mean(levcost, generation.weight)) %>%
-    ungroup() %>%
-    select(year, utility_code, plant_code, overnight_category, fuel_general, levcost)
+    ungroup()
 
-  levcst
+  levcst.weighted
 }
