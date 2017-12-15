6b4b15c844ac648abcc6eb46def9afe1f4b40a0f
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-12-15 13:09:19 -0500

    Match units output in Matteo COE paper

--- data-raw/costs/levelize.R
+++ data-raw/costs/levelize.R
@@ -2,20 +2,11 @@ calc.levelizedcosts <- function(capfactor, capcosts, map, fcr)
 {
 
   levcst <- capfactor %>%
-    inner_join(map, by=c("prime_mover", "fuel")) %>%  # attach overnight_category and fuel_general
-    inner_join(capcosts, by=c("year", "overnight_category")) %>% # attach three capital costs
+    inner_join(capcosts, by=c("yr", "overnightcategory")) %>% # attach overnight, om.var, om.fixed
     # levelize capital costs
-    mutate(overnight.lev = fcr * overnight / (8760 * capacityfactor),
-           om.fixed.lev = om.fixed / (8760 * capacityfactor),
-           cost.lev = overnight + om.fixed + om.var) %>%
-    select(year, utility_code, plant_code, prime_mover, fuel, overnight_category, fuel_general,
-           overnight.lev, om.fixed.lev, om.var, cost.lev) %>%
-    group_by(year, utility_code, plant_code, overnight_category, fuel_general) %>%
-    summarise(overnight.lev = mean(overnight.lev),
-              om.fixed.lev = mean(om.fixed.lev),
-              om.var = mean(om.var),
-              cost.lev = mean(cost.lev)) %>%
-    ungroup()
+    mutate(overnight.lev = fcr * (1000 * overnight) / (8760 * capacityfactor), # $/MWh
+           om.fixed.lev = (1000 * om.fixed) / (8760 * capacityfactor)) %>%  # $/MWh
+    select(yr, utilcode, plntcode, overnightcategory, fuel.general, overnight.lev, om.fixed.lev, om.var)
 
   levcst
 }
