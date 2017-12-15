29ada4a979decac1641ee44a863e51ff63b7375b
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-12-14 22:07:14 -0500

    new generators file

--- printshares.R
+++ printshares.R
@@ -3,13 +3,13 @@ library(magrittr)
 library(dplyr)
 library(ggplot2)
 
-fuel.markets <- marketshares(form860processed, mapping, "fuel")
-fuel.markets.new <- marketshares(form860processed, mapping, "fuel", additions=TRUE)
+fuel.markets <- marketshares(generators, mapping, "fuel")
+fuel.markets.new <- marketshares(generators, mapping, "fuel", additions=TRUE)
 ggsave("fueladditions.png")
 write.csv(fuel.markets.new, "fueladditions.csv", row.names=FALSE)
 
 
-tech.markets <- marketshares(form860processed, mapping, "tech")
-tech.markets.new <- marketshares(form860processed, mapping, "tech", additions=TRUE)
+tech.markets <- marketshares(generators, mapping, "tech")
+tech.markets.new <- marketshares(generators, mapping, "tech", additions=TRUE)
 ggsave("techadditions.png")
 write.csv(tech.markets.new, "techadditions.csv", row.names=FALSE)
