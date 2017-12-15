3024358d80be100208e5f5ff7174544c083ff861
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-12-14 22:03:28 -0500

    match according to category and fuel.general

--- data-raw/costs/capacityfactors.R
+++ data-raw/costs/capacityfactors.R
@@ -1,21 +1,22 @@
 calc.capacityfactors <- function(generators, plantgeneration)
 {
   plantcapacity <- generators %>%
-    select(year, summer_capacity, utility_code, plant_code, generator_code, prime_mover, fuel) %>%
-    rename(capacity = summer_capacity) %>%
-    group_by(year, utility_code, plant_code, prime_mover, fuel) %>%
+    select(yr, nameplate, utilcode, plntcode, gencode, overnightcategory, fuel.general) %>%
+    rename(capacity = nameplate) %>%
+    group_by(yr, utilcode, plntcode, overnightcategory, fuel.general) %>%
     summarise(capacity=sum(capacity)) %>%
     ungroup()
 
   plantgeneration <- plantgeneration %>%
-    select(year, generation, utility_code, plant_code, prime_mover, fuel) %>%
+    select(yr, generation, utilcode, plntcode, overnightcategory, fuel.general) %>%
     filter(generation > 0) # drop generators that used more energy than they produced
 
   capacityfactors <- plantcapacity %>%
-    inner_join(plantgeneration, by=c("year", "utility_code", "plant_code", "prime_mover", "fuel") ) %>%
+    inner_join(plantgeneration, by=c("yr", "utilcode", "plntcode", "overnightcategory", "fuel.general") ) %>%
     mutate(potentialgeneration = capacity * 8760) %>%
     mutate(capacityfactor = generation/potentialgeneration) %>%
-    filter(capacityfactor < 1) # this could be erroneous records or the fact that we're using summercapacity instead of nameplate
+    filter(capacityfactor < 1) %>% # this could be erroneous records or the fact that we're using summercapacity instead of nameplate
+    select(yr, utilcode, plntcode, overnightcategory, fuel.general, capacityfactor)
 
   capacityfactors
 
