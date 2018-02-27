d46cd1e08f66bff9063616cc708fa2a2e7405825
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-02-27 15:10:48 -0500

    cap.gen.join required dropping utilcode. Update levelization

--- data-raw/costs/levelize.R
+++ data-raw/costs/levelize.R
@@ -5,7 +5,7 @@ calc.levelizedcosts <- function(capfactor, capcosts, map, fcr)
     inner_join(capcosts, by=c("yr", "overnightcategory")) %>% # attach overnight, om.var, om.fixed
     mutate(overnight.lev = fcr * (1000 * overnight) / (8760 * capacityfactor), # $/MWh
            om.fixed.lev = (1000 * om.fixed) / (8760 * capacityfactor)) %>%  # $/MWh
-    select(yr, utilcode, plntcode, overnightcategory, fuel.general, overnight.lev, om.fixed.lev, om.var)
+    select(yr, plntcode, overnightcategory, fuel.general, overnight.lev, om.fixed.lev, om.var)
 
   levcst
 }
