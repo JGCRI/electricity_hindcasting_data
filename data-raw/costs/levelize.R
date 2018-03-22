16a425489873ce3d544ef92591c8b77d2aa4dcf8
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-22 16:08:20 -0400

    inclued lcoe column (sums overnight.lev, om.fixed.lev, and om.var)

--- data-raw/costs/levelize.R
+++ data-raw/costs/levelize.R
@@ -4,7 +4,8 @@ calc.levelizedcosts <- function(capfactor, capcosts, map, fcr)
   levcst <- capfactor %>%
     inner_join(capcosts, by=c("yr", "overnightcategory")) %>% # attach overnight, om.var, om.fixed
     mutate(overnight.lev = fcr * (1000 * overnight) / (8760 * capacityfactor), # $/MWh
-           om.fixed.lev = (1000 * om.fixed) / (8760 * capacityfactor)) %>%  # $/MWh
+           om.fixed.lev = (1000 * om.fixed) / (8760 * capacityfactor),
+           lcoe = overnight.lev + om.fixed.lev + om.var) %>%  # $/MWh
     select(-overnight, -om.fixed, -capacityfactor)
 
   levcst
