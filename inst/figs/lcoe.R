3aa6a3dc88ebc5ce0b55e7b284db49c6b16d7fb1
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-22 16:15:27 -0400

    implement summaryCalc()

--- inst/figs/lcoe.R
+++ inst/figs/lcoe.R
@@ -3,57 +3,12 @@ library(tidyr)
 library(ggplot2)
 library(ggridges)
 data(master, mapping)
-master <- master %>%
-  mutate(cshare = capacity / sum(capacity),
-         LCOE = overnight.lev + om.fixed.lev + om.var) %>%
-  select(-overnight.lev, -om.fixed.lev, -om.var, -marginal.cost, -generation)
 
+# industry-wide -----------------------------------------------------------
+lcoe.summary <- summaryCalc(master, lcoe)
 
-# industry-wide summary calculation ---------------------------------------
-
-summary <- master %>%
-  group_by(yr, overnightcategory, fuel.general) %>%
-  summarise(LCOE = weighted.mean(LCOE, capacity)) %>%
-  ungroup()
-
-# grab 21 (oc, fg) combos we have a mapping for
-mapping <- mapping %>%
-  select(overnightcategory, fuel.general) %>%
-  distinct() %>%
-  mutate_all(as.character)
-
-# force row for all possible (oc, fg, yr) combos: 130 possible
-# keep only those (oc, fg) combos that appear in the mapping: 16 observed in data
-summary.complete <- summary %>%
-  complete(overnightcategory, fuel.general, yr) %>%
-  inner_join(mapping, by=c("overnightcategory", "fuel.general"))
-
-# make summary.complete have two time series:
-## one with actual LCOE data
-## the other holds mean(LCOE) wherever there is missing data
-summary.complete.avg <- summary.complete %>%
-  group_by(fuel.general, overnightcategory) %>%
-  summarise(LCOE = mean(LCOE, na.rm=TRUE)) %>%
-  ungroup()
-summary.complete.na <- summary.complete %>%
-  filter(is.na(LCOE)) %>%
-  select(-LCOE) %>%
-  left_join(summary.complete.avg, by=c("fuel.general", "overnightcategory")) %>%
-  rename(missing=LCOE)
-summary.complete <- summary.complete %>%
-  left_join(summary.complete.na, by=c("yr", "fuel.general", "overnightcategory"))
-
-# n=16 for all years indicates all oc-fg combos appear in each yr
-summary.yr <- summary.complete %>%
-  group_by(yr) %>%
-  summarise(n=n()) %>%
-  ungroup()
-
-
-# industry-wide plot ------------------------------------------------------
-
-ggplot(summary.complete, aes(x=yr)) +
-  geom_line(aes(y = LCOE)) +
+ggplot(lcoe.summary, aes(x=yr)) +
+  geom_line(aes(y = lcoe)) +
   geom_point(aes(y = missing)) +
   facet_wrap(~fuel.general + overnightcategory, scales="free") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
@@ -64,14 +19,14 @@ ggsave("inst/figs/lcoe.avg.png", device="png",
 
 # histograms --------------------------------------------------------------
 
-ggplot(master, aes(x=LCOE)) + xlab("2010$/MWh") +
+ggplot(master, aes(x=lcoe)) + xlab("2010$/MWh") +
   geom_histogram(aes(fill = ..count..)) +
   facet_wrap(~fuel.general+overnightcategory, scales="free") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 ggsave("inst/figs/lcoe-hists/industry.png", device="png",
        width=8.5, height=8.5, units="in")
 
-ggplot(master, aes(x=log10(LCOE))) + xlab("log10(2010$/MWh)") +
+ggplot(master, aes(x=log10(lcoe))) + xlab("log10(2010$/MWh)") +
   geom_histogram(aes(fill = ..count..)) +
   facet_wrap(~fuel.general+overnightcategory, scales="free")
 ggsave("inst/figs/lcoe-hists/industry(logx).png",device="png",
