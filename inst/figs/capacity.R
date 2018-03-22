ff996384728cce333972f99bf56ce15c881246ea
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-22 15:57:26 -0400

    Apply summaryCalc() to master for capacity and lcoe

--- inst/figs/capacity.R
+++ inst/figs/capacity.R
@@ -1,127 +1,125 @@
-library(energy.markets)
-library(dplyr)
-library(magrittr)
-library(ggplot2)
+library(ElectricityHindcastingData)
 library(tidyr)
+library(ggplot2)
+library(ggridges)
+library(magrittr)
+library(dplyr)
+library(rlang)
+data(master, mapping)
 
-# Functions ---------------------------------------------------------------
-elec_tech_colors <- c( "coal" = "#a0237c",
-                       "b Coal w/CCS" = "#dab4c7",
-                       "gas" = "#25a9e0",
-                       "d Gas w/CCS" = "#84e7f9",
-                       "petroleum" = "#d01c2a",
-                       "f Oil w/CCS" = "#f7988f",
-                       "biomass" = "#00931d",
-                       "h Biomass w/CCS" = "#88c892",
-                       "nuclear" = "#ef8e27",
-                       "geothermal" = "#ad440c",
-                       "water" = "#fdfa28",
-                       "wind" = "#3d86f9",
-                       "solar" = "#fdd67b",
-                       "MSW" = "#507fab", # n CHP
-                       "other" = "#92a75d", # o Battery
-                       "energy reduction" = "grey")
-# display & save stacked bar plot
-plot <- function(df, ptitle) {
+# industry-wide summary ---------------------------------------------------
 
-  p <- ggplot(df, aes(x=yr, y=nameplate)) +
-    ggtitle(ptitle) +
-    theme(axis.text.x = element_text(angle = 45, hjust=1)) +
-    ylab("MW") +
+capacity.summary <- summaryCalc(master, capacity)
+
+# barchart (w/ points on period average for missing data)
+ggplot(capacity.summary, aes(x=yr)) + ylab("MW") + ggtitle("average plant capacity") +
+  geom_bar(aes(y = capacity), stat="identity") +
+  geom_point(aes(y = missing)) +
+  facet_wrap(~fuel.general + overnightcategory, scales="free") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("inst/figs/capacity.avg.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+# stacked barchart
+ggplot(capacity.summary, aes(x=yr, y=capacity)) + ylab("MW") + ggtitle("average capacity") +
+  geom_bar(aes(fill=fuel.general), stat="identity") +
+  scale_y_continuous(labels = scales::scientific) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("inst/figs/capacity.avg.stacked.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+# total industry capacity over the years ----------------------------------
+ggplot(master, aes(x=yr, y=nameplate)) + ylab("MW") + ggtitle("total capacity") +
+  geom_bar(stat="identity") +
+  facet_wrap(~fuel.general + overnightcategory, scales="free") +
+  scale_y_continuous(labels = scales::scientific) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("inst/figs/capacity.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+# stacked barchart
+ggplot(master, aes(x=yr, y=nameplate)) + ylab("MW") + ggtitle("total capacity") +
+  geom_bar(aes(fill=fuel.general), stat="identity") +
+  scale_y_continuous(labels = scales::scientific) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("inst/figs/capacity.stacked.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+
+
+# histograms over the years (by fuel) -------------------------------------
+
+
+ggplot(master, aes(x=nameplate)) + xlab("MW") +
+  geom_histogram(aes(fill = ..count..)) +
+  facet_wrap(~fuel.general + overnightcategory, scales="free")
+ggsave("inst/figs/capacity-hists/industry.png", device="png",
+       width=8.5, height=8.5, units="in")
+
+ggplot(master, aes(x=log10(nameplate))) + xlab("log10(MW)") +
+  geom_histogram(aes(fill = ..count..)) +
+  facet_wrap(~fuel.general + overnightcategory, scales="free")
+ggsave("inst/figs/capacity-hists/industry(logx).png",device="png",
+       width=8.5, height=8.5, units="in")
+
+fuels <- unique(master$fuel.general)
+lapply(fuels, function(fuel) { # 1 fuel, 1 plot: faceted by year
+  print(paste0("Plot: ", fuel))
+
+  df <- filter(master, fuel.general == fuel)
+
+  p <- ggplot(df, aes(x=nameplate)) + xlab("MW") +
+    geom_histogram(aes(fill = ..count..)) +
+    facet_grid(yr~., scales="fixed") +
     scale_y_continuous(labels = scales::scientific) +
-    scale_fill_manual(values=elec_tech_colors)
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    ggtitle(fuel)
+  fn <- paste0("inst/figs/capacity-hists/by year/", fuel, ".png")
+  ggsave(fn, plot=p, device="png", width=8.5, height=17, units="in")
 
+  p.log <- ggplot(df, aes(x=log10(nameplate))) + xlab("log10(MW)") +
+    geom_histogram(aes(fill = ..count..)) +
+    facet_grid(yr~., scales="fixed") +
+    scale_y_continuous(labels = scales::scientific) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    ggtitle(fuel)
+  fn <- paste0("inst/figs/capacity-hists/by year (logx)/", fuel, ".png")
+  ggsave(fn, plot=p.log, device="png", width=8.5, height=17, units="in")
 
-  # geom_bar()
-  if (grepl("fuel", ptitle)) {
-    p <- p +
-      geom_bar(aes(fill=fuel), stat="identity")
-  } else if (grepl("overnight", ptitle)) {
-    p <- p +
-      geom_bar(aes(fill=overnight), stat="identity")
-  }
-
-  return(p)
-}
-
-# aggregate by fuel/overnight, then plot
-plot.agg <- function(df, ptitle, group) {
-  df.grp <- df %>%
-    group_by_at(vars(yr, matches(group))) %>%
-    summarise(nameplate=sum(nameplate)) %>%
-    ungroup()
-
-  df.grp$yr <- factor(df.grp$yr, levels=seq(from=1990, to=2016, by=1))
-  if (group=="fuel") {
-    df.grp <- df.grp %>%
-      group_by(yr, fuel) %>%
-      complete(fuel, yr, fill=list(nameplate=0))
-  } else if (group=="overnight") {
-    df.grp <- df.grp %>%
-      group_by(yr, overnight) %>%
-      complete(overnight, yr, fill=list(nameplate=0))
-  } else {
-    stop("Group not recognized!")
-  }
-
-  ptitle <- paste0(ptitle, " by ", group)
-  p <- plot(df.grp, ptitle)
-  return(p)
-}
-
-# multiplot w/ one legend function
-source("figs/sharedlegend.R")
-# 1 2
-# 3 4
-
-# multiplot w/ multiple legends
-source("figs/multiplot.R")
-# 1 3
-# 2 4
+})
 
 
 
-# Original CAP Dataset --------------------------------------------------------
-data(capacity.unmapped, mapping)
+# ridgeline plots (faceted by year) (by fuel) -----------------------------
 
-# full fleet
-cap <- capacity.unmapped %>%
-  left_join(mapping, by=c("primemover", "fuel")) %>%
-  group_by(yr, fuel.general, overnightcategory) %>%
-  summarise(nameplate=sum(nameplate)) %>%
-  ungroup() %>%
-  dplyr::rename(fuel = fuel.general,
-                overnight = overnightcategory) %>%
-  mutate(yr = as.factor(yr))
+ggplot(master, aes(x=nameplate, y=fuel.general, fill=fuel.general)) + xlab("MW") +
+  geom_density_ridges() +
+  theme_ridges()
+ggsave("inst/figs/capacity-ridges/industry.png", device="png")
 
-# get plots
-cap.fuel <- plot.agg(cap, "CAP", "fuel")
+ggplot(master, aes(x=log10(nameplate), y=fuel.general, fill=fuel.general)) + xlab("log10(MW)") +
+  geom_density_ridges() +
+  theme_ridges()
+ggsave("inst/figs/capacity-ridges/industry(logx).png", device="png")
 
+lapply(fuels, function(fuel) { # 1 fuel, 1 plot: faceted by year
+  df <- master %>%
+    filter(fuel.general==fuel) %>%
+    mutate(yr = as.factor(yr))
 
-# joined dataset ----------------------------------------------------------
-data(cap.gen.joined)
+  print(paste0("Plot: ", fuel))
 
-# full fleet
-join <- cap.gen.joined %>%
-  group_by(yr, fuel.general, overnightcategory) %>%
-  summarise(nameplate = sum(nameplate)) %>%
-  ungroup() %>%
-  dplyr::rename(fuel = fuel.general,
-                overnight = overnightcategory) %>%
-  mutate(yr = as.factor(yr))
+  p <- ggplot(df, aes(x=nameplate, y=yr)) + xlab("MW") +
+    geom_density_ridges() +
+    theme_ridges() +
+    ggtitle(fuel)
+  fn <- paste0("inst/figs/capacity-ridges/by year/", fuel, ".png")
+  ggsave(fn, plot=p, device="png")
 
-
-# get plots
-join.fuel <- plot.agg(join, "JOIN", "fuel")
-
-
-# PLOTS -------------------------------------------------------------------
-
-## SAVE FUEL PLOT
-fn <- "figs/Capacity by fuel.png"
-print(paste0("saving ", fn))
-png(fn, width=11, height=17, units="in", res=250)
-grid_arrange_shared_legend(orig.fuel, join.fuel,
-                           position="right", ncol=2, nrow=1)
-dev.off()
-
+  p.log <- ggplot(df, aes(x=log10(nameplate), y=yr)) + xlab("log10(MW)") +
+    geom_density_ridges() +
+    theme_ridges() +
+    ggtitle(fuel)
+  fn <- paste0("inst/figs/capacity-ridges/by year (logx)/", fuel, ".png")
+  ggsave(fn, plot=p, device="png")
+})
