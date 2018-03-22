598970a5ab15c2f0c0de76518256a057513c954b
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-22 16:59:35 -0400

    Implement summaryPlot()

--- inst/figs/summary.R
+++ inst/figs/summary.R
@@ -8,25 +8,6 @@ library(rlang)
 data(master, mapping)
 
 
-# average plant capacity --------------------------------------------------
-capacity.summary <- summaryCalc(master, capacity)
-
-# barchart (w/ points on period average for missing data)
-ggplot(capacity.summary, aes(x=yr)) + ylab("MW") + ggtitle("average plant capacity") +
-  geom_bar(aes(y = capacity), stat="identity") +
-  geom_point(aes(y = missing)) +
-  facet_wrap(~fuel.general + overnightcategory, scales="free") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("inst/figs/capacity.avg.png", device="png",
-       width = 8.5, height = 8.5, units = "in")
-
-# stacked barchart
-ggplot(capacity.summary, aes(x=yr, y=capacity)) + ylab("MW") + ggtitle("average capacity") +
-  geom_bar(aes(fill=fuel.general), stat="identity") +
-  scale_y_continuous(labels = scales::scientific) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("inst/figs/capacity.avg.stacked.png", device="png",
-       width = 8.5, height = 8.5, units = "in")
 
 # total sector capacity ---------------------------------------------------
 
@@ -47,15 +28,21 @@ ggsave("inst/figs/capacity.stacked.png", device="png",
        width = 8.5, height = 8.5, units = "in")
 
 
-# average plant lcoe  -----------------------------------------------------
-lcoe.summary <- summaryCalc(master, lcoe)
 
-ggplot(lcoe.summary, aes(x=yr)) +
-  geom_line(aes(y = lcoe)) +
-  geom_point(aes(y = missing)) +
-  facet_wrap(~fuel.general + overnightcategory, scales="free") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  ylab("2010$/MWh") +
-  ggtitle("average levelized cost of energy")
+
+# average summary time-series ---------------------------------------------
+
+capacity.summary <- summaryCalc(master, capacity)
+summaryPlot(capacity.summary, capacity, "MW")
+ggsave("inst/figs/capacity.avg.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+generation.summary <- summaryCalc(master, generation)
+summaryPlot(generation.summary, generation, "MWh")
+ggsave("inst/figs/generation.avg.png", device="png",
+       width = 8.5, height = 8.5, units = "in")
+
+lcoe.summary <- summaryCalc(master, lcoe)
+summaryPlot(lcoe.summary, lcoe, "2010$/MWh")
 ggsave("inst/figs/lcoe.avg.png", device="png",
        width = 8.5, height = 8.5, units = "in")
