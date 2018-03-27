d024a817a36362c8f19fc4933056c18a7d29d449
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-27 17:50:51 -0400

    Match ridgelines plot size to histograms

--- inst/figs/ridgelines.R
+++ inst/figs/ridgelines.R
@@ -32,14 +32,14 @@ spread_by_year <- function(fuel, df) { # 1 fuel, 1 plot: faceted by year
     theme_ridges() +
     ggtitle(fuel)
   fn <- paste0("inst/figs/capacity-ridges/by year/", fuel, ".png")
-  ggsave(fn, plot=p, device="png")
+  ggsave(fn, plot=p, device="png", width=8.5, height=17, units="in")
 
   p.log <- ggplot(df, aes(x=log10(capacity), y=yr)) + xlab("log10(MW)") +
     geom_density_ridges() +
     theme_ridges() +
     ggtitle(fuel)
   fn <- paste0("inst/figs/capacity-ridges/by year (logx)/", fuel, " (logx).png")
-  ggsave(fn, plot=p.log, device="png")
+  ggsave(fn, plot=p.log, device="png", width=8.5, height=17, units="in")
 }
 fuels <- unique(master$fuel.general)
 lapply(fuels, spread_by_year, master)
