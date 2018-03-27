dce567d5b29eeafb54948d26f6897609356c3b5c
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-27 14:03:00 -0400

    Find years where either CAP or GEN is missing. Same results for ORIG & MAP, but somehow data is found for JOIN

--- inst/figs/missing.R
+++ inst/figs/missing.R
@@ -45,9 +45,15 @@ map <- function(df, column) {
 }
 
 my_full_join <- function(df1, df2) {
-  full_join(df1, df2, by = c("overnightcategory", "fuel.general", "yr"))
+  full_join(df1, df2, by = c("fuel.general", "overnightcategory", "yr"))
 }
 
+select_missing <- function(df) {
+  df %>%
+    filter(yr >= 1996) %>% # capital cost data begins @ 96
+    filter(!is.na(missing.x) | ! is.na(missing.y)) %>%
+    arrange(fuel.general, overnightcategory, yr)
+}
 
 # get summary time-series  ------------------------------------------------
 
@@ -72,22 +78,38 @@ master.gen.summary <- master %>% summaryCalc(generation)
 # bind CAP/GEN summary-time series  ---------------------------------------
 
 # easier to compare locations of missing values in single table
-orig.summary <- my_full_join(orig.cap.summary, orig.gen.summary)
-map.summary <- my_full_join(map.cap.summary, map.gen.summary)
-join.summary <- my_full_join(join.cap.summary, join.gen.summary)
-master.summary <- my_full_join(master.cap.summary, master.gen.summary)
+orig.summary <- my_full_join(orig.cap.summary, orig.gen.summary) %>% select_missing()
+map.summary <- my_full_join(map.cap.summary, map.gen.summary) %>% select_missing()
+join.summary <- my_full_join(join.cap.summary, join.gen.summary) %>% select_missing()
+master.summary <- my_full_join(master.cap.summary, master.gen.summary) %>% select_missing()
 
 
-# inspect 00-01 -----------------------------------------------------------
 
-orig.summary %>% filter(yr %in% c(2000, 2001)) %>% View("ORIG (00-01)")
-map.summary %>% filter(yr %in% c(2000, 2001)) %>% View("MAP (00-01)")
+# inspect missing data ----------------------------------------------------
+
+# these fg-oc pairs are missing data just for 2001 & 2002
+orig.summary %>%
+  group_by(fuel.general, overnightcategory) %>%
+  filter(unique(yr) == c(2001, 2002)) %>%
+  View("ORIG")
+
+# the same pairs appear in the mapped version of ORIG
+map.summary %>%
+  group_by(fuel.general, overnightcategory) %>%
+  filter(unique(yr) == c(2001, 2002)) %>%
+  View("MAP")
 
 # JOIN dataset somehow finds missing 2001 GEN data (but not 2000...)
-joined.summary %>% filter(yr %in% c(2000, 2001)) %>% View("JOIN (00-01)")
+# ^ what did this mean?
+
+join.summary %>%
+  # group_by(fuel.general, overnightcategory) %>%
+  # filter(unique(yr) == c(2001, 2002)) %>%
+  filter(length(unique(yr)) == 2) %>%
+  View("JOIN")
 
 # downstream effect
-master.summary %>% filter(yr %in% c(2000, 2001)) %>% View("Master (00-01)")
+master.summary %>% View("Master (00-01)")
 
 
 
