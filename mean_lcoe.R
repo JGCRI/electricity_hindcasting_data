793f879c8979b0bd4698fe1512b1263dfd71376c
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-05-03 12:40:29 -0400

    recreate plant-level lcoe file

--- mean_lcoe.R
+++ mean_lcoe.R
@@ -1,12 +1,54 @@
 library(ElectricityHindcastingData)
+library(ggplot2)
 
-data(cap.gen.joined, levelizedcosts)
 
-# drop from cap.gen.joined 1990-1995
-anti_join(cap.gen.joined, levelizedcosts) %>% filter(!overnightcategory %in% c("ST", "MSW")) %>% View()
+# CF ----------------------------------------------------------------------
+cf <- capacityfactors.data %>%
+  group_by(yr, fuel.general, overnightcategory) %>%
+  summarise(avg = mean(capacityfactor),
+            min = min(capacityfactor),
+            max = max(capacityfactor)) %>%
+  ungroup() %>%
+  arrange(fuel.general, overnightcategory, yr) %>%
+  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
+  select(-fuel.general, -overnightcategory) %>%
+  tidyr::gather(key=param, value=CF, -yr, -tech )
 
-# no data in levelizedcosts that's not in cap.gen.joined
-anti_join(levelizedcosts, cap.gen.joined) %>% View()
+ggplot(cf, aes(x=yr, y=CF, color=param)) +
+  geom_line() +
+  facet_wrap(~tech)
+
+capacityfactors.data %>%
+  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
+  ggplot(., aes(x=capacityfactor)) +
+  geom_histogram(binwidth=0.01) +
+  facet_wrap(~tech, scales="free")
+
+
+# LCOE --------------------------------------------------------------------
+levelizedcosts %>%
+  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
+  ggplot(., aes(x=capacityfactor, y=LCOE_wo_Fuel)) +
+  geom_point() +
+  facet_wrap(~tech, scales="free")
+
+levelizedcosts %>%
+  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
+  ggplot(., aes(x=yr, y=LCOE_wo_Fuel)) +
+  geom_point() +
+  facet_wrap(~tech, scales="free")
+
+
+
+
+
+
+# final_plant_level.xlsx --------------------------------------------------
+final_plant_lcoe <- levelizedcosts %>%
+  group_by(startyr, plntcode, fuel.general, overnightcategory) %>%
+  summarise(lcoe = mean(LCOE)) %>%
+  ungroup() %>%
+  filter( startyr %in% 1995:2015)
 
 
 # fuel-tech market shares -------------------------------------------------
@@ -45,34 +87,4 @@ lcoe_avg <- levelizedcosts %>%
 
 
 
-# plots -------------------------------------------------------------------
-
-library(ggplot2)
-
-cf <- capacityfactors.data %>%
-  group_by(yr, fuel.general, overnightcategory) %>%
-  summarise(avg = mean(capacityfactor),
-            min = min(capacityfactor),
-            max = max(capacityfactor)) %>%
-  ungroup() %>%
-  arrange(fuel.general, overnightcategory, yr) %>%
-  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
-  select(-fuel.general, -overnightcategory) %>%
-  tidyr::gather(key=param, value=CF, -yr, -tech )
-
-ggplot(cf, aes(x=yr, y=CF, color=param)) +
-  geom_line() +
-  facet_wrap(~tech)
-
-capacityfactors.data %>%
-  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
-  ggplot(., aes(x=capacityfactor)) +
-  geom_histogram(binwidth=0.01) +
-  facet_wrap(~tech, scales="free")
-
-levelizedcosts %>%
-  mutate(tech = paste0(fuel.general, "-", overnightcategory)) %>%
-  ggplot(., aes(x=capacityfactor, y=LCOE_wo_Fuel)) +
-  geom_point() +
-  facet_wrap(~tech, scales="free")
 
